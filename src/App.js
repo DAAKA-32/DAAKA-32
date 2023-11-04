7158202e6009315d3bcbd30fad5874766c600c47
Emilien Nepveu <emilien.nepveu@gmail.com>
2023-11-04 19:02:10 +0100

    Probleme

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/src/Page/Intro.js" element={<Intro />} />
-        <Route path="/src/Page/Home.js" element={<Home />} />
+        <Route path="./Page/Intro.js" element={<Intro />} />
+        <Route path="./Page/Home.js" element={<Home />} />
         <Route path="*" element={<Intro />} />
         {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
         {/* <Route path="*" element={<Home />} /> */}
--- src/Page/Intro.js
+++ src/Page/Intro.js
@@ -1,29 +1,35 @@
 import React from 'react';
-// import '../componentCSS/Intro.css';
+import { Link } from 'react-router-dom';
+// import '../componentCSS/Intro.css'; 
 
 const Intro = () => {
     return (
-    <div>
-        {/* <img src="../P-films.mp4" alt="Vidéo" /> */}
-        <div className="container">
-            <header>
-                <h1>Bienvenue sur P-film</h1>
-                <p>Le site des meilleurs films à voir entre amis !</p>
-            </header>
-            <main>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREmgOovGYnR3piBGJCFChBHtvyKCUSFRBzKg&usqp=CAU" alt="cinéma"/>
-            </main>
-            <footer>
-                <h2>Prêt à découvrir des mystères cinématographiques ?</h2>
-                <br />
-                <h4>La liste est longue...</h4>
-                <button>
-                    <a href="../Page/Home.js">Cliquez ici</a>
-                </button>
-            </footer>
-        </div>
+        <div>
+            <div className="container">
+                <header>
+                    <h1>Bienvenue sur P-film</h1>
+                    <p>Le site des meilleurs films à voir entre amis !</p>
+                </header>
+                <main>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREmgOovGYnR3piBGJCFChBHtvyKCUSFRBzKg&usqp=CAU" alt="cinéma"/>
+                </main>
+                <footer>
+                    <h2>Prêt à découvrir des mystères cinématographiques ?</h2>
+                    <br />
+                    <h4>La liste est longue...</h4>
+                    
+                    {/* Ajout du bouton de redirection vers une autre page */}
+                    <Link to="../Page/Home.js">Aller à une autre page</Link>
+                </footer>
+            </div>
         </div>
     );
 };
 
 export default Intro;
+
+
+
+// {/* <button>
+//                     <a href="../Page/Home.js">Cliquez ici</a>
+//                 </button> */
